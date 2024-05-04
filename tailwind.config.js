e2770df329d2556fd39b6d5987e3ba597303073a
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-05-05 02:21:07 +0530

    config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,85 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        slideInLeft: {
+          "0%": { opacity: 0, transform: "translateX(-30vw)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
+        },
+        slideInRight: {
+          "0%": { opacity: 0, transform: "translateX(30vw)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
+        },
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+        rotate: {
+          "0%": { transform: "rotate(0deg) scale(10)" },
+
+          "100%": { transform: "rotate(-360deg) scale(10)" },
+        },
+        slideUp: {
+          "0%": { opacity: 0, transform: "translateY(20vh)" },
+
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        slideUpSmall: {
+          "0%": { opacity: 0, transform: "translateY(5vh)" },
+
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        textTransform: {
+          "0%": {
+            transform: " translateZ(0px)  ",
+          },
+          "100%": {
+            transform: " translateZ(-50vh) ",
+          },
+        },
+        "text-reveal": {
+          "0%": {
+            transform: "translate(0, 100%)",
+          },
+          "100%": {
+            transform: "translate(0, 0)",
+          },
+        },
+        rotate1: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        "radar-spin": {
+          from: {
+            transform: "rotate(20deg)",
+          },
+          to: {
+            transform: "rotate(380deg)",
+          },
+        },
+      },
+      animation: {
+        rotate: "rotate 6s linear infinite",
+        slideUp: "slideUp 1s ease-in-out forwards",
+        slideUpSlow: "slideUp 2s ease-in-out forwards",
+        slideUpSmall: "slideUpSmall 0.5s ease-in-out forwards",
+        textTransform: "textTransform 2s ease-in-out forwards",
+        "text-reveal": "text-reveal 1.5s cubic-bezier(0.77, 0, 0.175, 1) 0.5s",
+        "radar-spin": "radar-spin 4s linear infinite",
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+        rotate1: "rotate1 18s linear infinite",
+        slideInLeft: "slideInLeft 1.5s ease-in-out forwards",
+        slideInRight: "slideInRight 1.5s ease-in-out forwards",
+      },
     },
   },
   plugins: [],
