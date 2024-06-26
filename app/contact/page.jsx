f814320f50e04ad212ffff3e9955244eed3c3979
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-06-26 17:39:58 +0530

    contact us changes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,40 +1,62 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import Transition from "@/components/Transition";
 import RevealOnScroll from "@/components/RevealOnScroll";
 import { alfa } from "../layout";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import emailjs from "@emailjs/browser";
 import useStore from "@/utils/store";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 
 const ContactUs = () => {
+  const [loading, setLoading] = React.useState(false);
+
   const [emailSent, setEmailSent] = React.useState(false);
-  const { register, handleSubmit, formState, reset } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
   const { currentPointer, setCurrentPointer } = useStore();
 
   const onSubmit = (data) => {
+    setLoading(true);
     emailjs
       .send(
         "service_vyc3aph",
         "template_vw3o9zp",
         {
-          from_name: "anirudh",
-          from_email: "test@gmail.com",
-          from_message: "abcddefgh",
+          from_name: data.name,
+          from_email: data.email,
+          from_phoneNumber: data.phoneNumber,
+          from_message: data.message,
         },
         {
-          publicKey: "8SoO2AD_OUoRwAQ7_ntbU",
+          publicKey: "VV1JBQAmRXz7mALR7",
         }
       )
       .then((res) => {
         setEmailSent(true);
+        setLoading(false);
         reset();
       })
       .catch((err) => {
+        setLoading(false);
         console.log("error", err);
       });
   };
+
+  useEffect(() => {
+    if (emailSent) {
+      setTimeout(() => {
+        setEmailSent(false);
+      }, 2000);
+    }
+  }, [emailSent]);
 
   return (
     <Transition>
@@ -43,29 +65,25 @@ const ContactUs = () => {
         <div className="circle -bottom-1/2 -right-1/2 absolute" />
         <div className="circle -bottom-1/2 left-0 absolute" />
 
-        <div
-          className={
-            "  max-h-[70vh] min-h-[70vh] h-[70vh] w-screen overflow-hidden"
-          }
-        >
-          <div className="flex flex-col w-full h-full items-start relative justify-start  py-[5vh] ">
+        <div className={"  h-full w-screen overflow-hidden"}>
+          <div className="flex flex-col w-full h-full items-start relative justify-start  py-[2.5vh] ">
             <RevealOnScroll
               addedClasses={
                 "flex flex-col items-center justify-center w-full p-8 animate-animateSlideUp"
               }
             >
               <h2
-                className={`${alfa.className} text-white text-[3rem] lg:text-[4rem] leading-[4rem] font-bold `}
+                className={`${alfa.className} text-white text-[2.5rem] lg:text-[4rem] leading-[4rem] font-bold `}
               >
                 CONTACT
               </h2>
               <p className="text-white text-center text-sm">
                 {
-                  " Reach out to us for any collorations or queries. We are always here to help you."
+                  " Reach out to us for any collaborations or queries. We are always here to help you."
                 }
               </p>
             </RevealOnScroll>
-            <div className=" flex flex-col items-center w-full mt-[5vh]">
+            <div className=" flex flex-col items-center w-full mt-4 lg:mt-[5vh]">
               <form
                 onSubmit={handleSubmit(onSubmit)}
                 className=" w-[80vw] lg:w-[40%] flex flex-col items-center justify-center"
@@ -73,25 +91,55 @@ const ContactUs = () => {
                 <div className="flex flex-col items-center w-full">
                   <input
                     type="text"
-                    placeholder="Name"
-                    {...register("name", { required: true })}
+                    placeholder="Name*"
+                    {...register("name", { required: true, minLength: 5 })}
                     className="mb-4 w-full bg-transparent placeholder:text-white/70 focus:bg-transparent text-white  text-xl border-b-[1px] border-red-800"
                   />
+                  {errors.name?.message ? (
+                    <p className=" text-xs text-red-500">
+                      {errors.name.message}
+                    </p>
+                  ) : null}
                 </div>
                 <div className="flex flex-col items-center w-full mt-4">
                   <input
                     type="email"
-                    placeholder="Email"
-                    {...register("email", { required: true })}
+                    placeholder="Email*"
+                    {...register("email", {
+                      required: true,
+                      minLength: 5,
+                      pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        message: "invalid email address",
+                      },
+                    })}
+                    className="mb-4 w-full placeholder:text-white/70 focus:bg-transparent text-white  bg-transparent text-xl border-b-[1px] border-red-800"
+                  />
+                  {errors.email?.message ? (
+                    <p className=" text-xs text-red-500">
+                      {errors.email.message}
+                    </p>
+                  ) : null}
+                </div>
+                <div className="flex flex-col items-center w-full mt-4">
+                  <input
+                    type="phoneNumber"
+                    placeholder="Phone Number"
+                    {...register("phoneNumber")}
                     className="mb-4 w-full placeholder:text-white/70 focus:bg-transparent text-white  bg-transparent text-xl border-b-[1px] border-red-800"
                   />
                 </div>
                 <div className="flex flex-col items-center w-full mt-4">
                   <textarea
-                    placeholder="Message"
+                    placeholder="Message*"
                     {...register("message", { required: true })}
                     className="mb-4 w-full placeholder:text-white/70 focus:bg-transparent text-white  h-[10vh] bg-transparent text-xl border-b-[1px] border-red-800"
                   />
+                  {errors.message?.message ? (
+                    <p className=" text-xs text-red-500">
+                      {errors.message.message}
+                    </p>
+                  ) : null}
                 </div>
                 {emailSent ? (
                   <button
@@ -103,6 +151,7 @@ const ContactUs = () => {
                   </button>
                 ) : (
                   <button
+                    disabled={errors.name || errors.email || errors.message}
                     type="submit"
                     onMouseEnter={() => {
                       setCurrentPointer("a");
@@ -110,13 +159,64 @@ const ContactUs = () => {
                     onMouseLeave={() => {
                       setCurrentPointer("");
                     }}
-                    className="bg-red-800 text-white px-8 py-2 mt-8 "
+                    className="bg-red-800 disabled:bg-gray-700 text-white px-8 py-2 mt-2 z-20 lg:mt-8 "
                   >
-                    Submit
+                    {loading ? "..." : "Submit"}
                   </button>
                 )}
               </form>
             </div>
+          </div>
+        </div>
+
+        <div className=" w-3/12 flex flex-row justify-center items-center gap-x-2 my-2 lg:my-8">
+          <div className="h-[1px] bg-white w-[45%]  border-white" />
+          <p className=" text-xl text-white">OR</p>
+          <div className="h-[1px] bg-white w-[45%]  border-white" />
+        </div>
+
+        <div className="flex flex-col items-center w-11/12 lg:w-6/12">
+          <p>Reach out to us on our Social media</p>
+          <div className=" w-4/12 flex flex-row justify-between mt-8">
+            <a
+              onMouseEnter={() => {
+                setCurrentPointer("a");
+              }}
+              onMouseLeave={() => {
+                setCurrentPointer("");
+              }}
+              href="https://www.instagram.com/doleshwar_raj/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaInstagram size={24} color="#fff" />
+            </a>
+            <a
+              onMouseEnter={() => {
+                setCurrentPointer("a");
+              }}
+              onMouseLeave={() => {
+                setCurrentPointer("");
+              }}
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebook size={24} color="#fff" />
+            </a>
+            <a
+              onMouseEnter={() => {
+                setCurrentPointer("a");
+              }}
+              onMouseLeave={() => {
+                setCurrentPointer("");
+              }}
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaTwitter size={24} color="#fff" />
+            </a>
           </div>
         </div>
       </div>
