e5eb740dd751df6ee58c190291c26f484454ec2a
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-10-09 22:37:41 +0530

    consts changes

--- utils/consts.js
+++ utils/consts.js
@@ -233,10 +233,16 @@ export const pop = [
     imageUrl: process.env.NEXT_PUBLIC_API_URL + "/dolafz-dolesmusic.webp",
   },
   {
+    artist: "SSD & Ajay Tiwari",
     song: "Guftagoo",
-    Spotify: "",
-    AppleMusic: "",
-    Youtube: "",
+    Producer: "",
+    Mix: "",
+    Master: "",
+    Spotify:
+      "https://open.spotify.com/track/4JhdGL0DfYIa4dQn2kLLpl?si=9d37ffe5a3384f9ahttps://open.spotify.com/track/4JhdGL0DfYIa4dQn2kLLpl?si=9d37ffe5a3384f9a",
+    AppleMusic:
+      "https://music.apple.com/in/album/guftagoo/1762876206?i=1762876207",
+    Youtube: "https://youtu.be/9Tg7xxVV0F4?si=QR8VWGPy0tEaTw5L",
     imageUrl: process.env.NEXT_PUBLIC_API_URL + "/art3.jpeg",
   },
   {
@@ -288,6 +294,19 @@ export const pop = [
 ];
 
 export const rock = [
+  {
+    artist: "Param & Doleshwar Raj",
+    song: "Gumshuda",
+    Producer: "",
+    Mix: "",
+    Master: "",
+    Spotify:
+      "https://open.spotify.com/track/0QY7klMVF94B8YdyALjtP0?si=bf73f9fd12c34517",
+    AppleMusic:
+      "https://music.apple.com/in/album/gumshuda/1770334934?i=1770334935",
+    Youtube: "https://youtu.be/xUXW4VvRYPw?si=t1Y_d2rfKfO5DRFD",
+    imageUrl: process.env.NEXT_PUBLIC_API_URL + "/gumshuda.jpg",
+  },
   {
     artist: "Atrangi Funkaar",
     song: "Super Mario",
@@ -495,6 +514,32 @@ export const hiphop = [
 
 export const upcoming = [
   {
+    artist: "Param & Doleshwar Raj",
+    song: "Gumshuda",
+    Producer: "",
+    Mix: "",
+    Master: "",
+    Spotify:
+      "https://open.spotify.com/track/0QY7klMVF94B8YdyALjtP0?si=bf73f9fd12c34517",
+    AppleMusic:
+      "https://music.apple.com/in/album/gumshuda/1770334934?i=1770334935",
+    Youtube: "https://youtu.be/xUXW4VvRYPw?si=t1Y_d2rfKfO5DRFD",
+    imageUrl: process.env.NEXT_PUBLIC_API_URL + "/gumshuda.jpg",
+  },
+  {
+    artist: "SSD & Ajay Tiwari",
+    song: "Guftagoo",
+    Producer: "",
+    Mix: "",
+    Master: "",
+    Spotify:
+      "https://open.spotify.com/track/4JhdGL0DfYIa4dQn2kLLpl?si=9d37ffe5a3384f9ahttps://open.spotify.com/track/4JhdGL0DfYIa4dQn2kLLpl?si=9d37ffe5a3384f9a",
+    AppleMusic:
+      "https://music.apple.com/in/album/guftagoo/1762876206?i=1762876207",
+    Youtube: "https://youtu.be/9Tg7xxVV0F4?si=QR8VWGPy0tEaTw5L",
+    imageUrl: process.env.NEXT_PUBLIC_API_URL + "/art3.jpeg",
+  },
+  {
     song: "2 Lafz",
     artist: "SSD, Mayyank Solanki, Garvit Soni",
     Producer: "Doleshwar Raj & Mayyank Solanki",
@@ -561,454 +606,7 @@ export const upcoming = [
     released: true,
     imageUrl: process.env.NEXT_PUBLIC_API_URL + "/rozana1500w.jpg",
   },
-  // {
-  //   song: "Kiddies",
-  //   artist: "Fat Guyy",
-  //   released: true,
-  //   Spotify:
-  //     "https://open.spotify.com/track/0RuUKK6PciL1af8euocNQD?si=b8b936332a8246a6",
-  //   Producer: "Doleshwar Raj",
-  //   Mix: "Gautam Khandelwal",
-  //   Spotify:
-  //     "https://open.spotify.com/track/0RuUKK6PciL1af8euocNQD?si=b8b936332a8246a6",
-  //   AppleMusic:
-  //     "https://music.apple.com/in/album/kiddies/1744394684?i=1744394685",
-  //   Youtube:
-  //     "https://music.youtube.com/watch?v=3k7e0HFlWIs&si=IJekO8igcFYXpO_l",
-  //   imageUrl:
-  //     process.env.NEXT_PUBLIC_API_URL +
-  //     "/SmartSelect_20240505_154021_YouTube_Music.jpg",
-  // },
 ];
-
-// export const songsAtrangi = [
-//   {
-//     artist: "Akshay Agarwal, Hardik Keshan",
-//     song: "The Alien Song",
-//     Spotify:
-//       "https://open.spotify.com/track/7KJttnRWOBs8bj5VVKiAND?si=668ee47f6652424f",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/the-alien-song/1614506887?i=1614506903",
-//     Youtube: "https://youtu.be/cSA8gEqgqQE?si=oPVm8strFGKb5va2",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_153651_YouTube_Music.jpg",
-//   },
-//   {
-//     artist: "Akshay Agarwal, Hardik Keshan",
-//     song: "Chor Do Dheela",
-//     Spotify:
-//       "https://open.spotify.com/track/734U2Bp1FXV1Amqtvx6VS1?si=98f17c587ae145f1",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/chor-do-dheela/1626913801?i=1626914386",
-//     Youtube: "https://youtu.be/bjoaiQzCcbI?si=_Zkb5A87SmQ_9-G8",
-//     imageUrl: "",
-//   },
-//   {
-//     artist: "Atrangi Funkaar",
-//     song: "Super Mario",
-//     Spotify:
-//       "https://open.spotify.com/track/6WXzHjZNw0QcCpj2ejQAXc?si=6dbf6f8101964a73",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/supermario/1607268192?i=1607268249",
-//     Youtube: "https://youtu.be/v5k7XRtM4q4?si=I15oGmtApaaMg4Ro",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_153733_YouTube_Music.jpg",
-//   },
-//   {
-//     artist: "Atrangi Funkaar",
-//     song: "Uncle Ji",
-//     Spotify:
-//       "https://open.spotify.com/track/1yADWs1m6pBVoV72a5SmOP?si=aecbd8acbacc498a",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/uncle-ji/1589233045?i=1589233051",
-//     Youtube: "https://youtu.be/DGdyrkDcfmU?si=ePLZJY4Ogi_XuCNe",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_153538_YouTube_Music.jpg",
-//   },
-//   {
-//     artist: "Atrangi Funkaar",
-//     song: "Kho Gayi",
-//     Spotify:
-//       "https://open.spotify.com/track/6NYVlIbWhqJZu3XkoA0JZu?si=a7875c42950e4cbb",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/kho-gayi/1544476467?i=1544477247",
-//     Youtube: "https://youtu.be/JSjhTWikNrA?si=150mqB0jvXYYw--B",
-//     imageUrl: "",
-//   },
-//   {
-//     artist: "Atrangi Funkaar",
-//     song: "Kushhboo",
-//     Spotify:
-//       "https://open.spotify.com/track/3W1OETYkvZezs2k3duLVIC?si=6566799f9eb9449e",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/khushboo/1677529455?i=1677529907",
-//     Youtube: "https://youtu.be/aGRGvS-WKe4?si=E4Egj33mEXGRQcDK",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_155607_Spotify.jpg",
-//   },
-//   {
-//     artist: "Atrangi Funkaar",
-//     song: "Tanne Taadu",
-//     Spotify:
-//       "https://open.spotify.com/track/77pRY7SqpN4Gga4KliHnHX?si=e0954fe30f2445a7",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/tanne-taadu/1648022679?i=1648022680",
-//     Youtube: "https://youtu.be/EC2T86QBvaE?si=CKQgc3vy0NgxgbQY",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_155530_Spotify.jpg",
-//   },
-//   {
-//     artist: "Atrangi Funkaar",
-//     song: "Gansgter",
-//     Spotify:
-//       "https://open.spotify.com/track/6b5KEoabBAZTy4z3NqBcXw?si=fdd6238f96be4c14",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/gangster/1677529455?i=1677529908",
-//     Youtube: "https://youtu.be/bMMowtwQ8gg?si=-TyZFq6IldtcgwzY",
-//     imageUrl: "",
-//   },
-//   {
-//     artist: "Atrangi Funkaar",
-//     song: "Paravasha - Pancham Jeeva",
-//     Spotify:
-//       "https://open.spotify.com/track/73xfwYEsymSfVofTD6OKfs?si=358647517e664a06",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/paravasha-from-grey-games/1737907731?i=1737907743",
-//     Youtube: "https://youtu.be/bjoaiQzCcbI?si=g0J1nX6hC597qjzZ",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_154722_Spotify.jpg",
-//   },
-//   {
-//     artist: "Atrangi Funkaar",
-//     song: "Na Rahi Rahe",
-//     Spotify:
-//       "https://open.spotify.com/track/73xfwYEsymSfVofTD6OKfs?si=358647517e664a06",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/paravasha-from-grey-games/1737907731?i=1737907743",
-//     Youtube: "https://youtu.be/bjoaiQzCcbI?si=g0J1nX6hC597qjzZ",
-//     imageUrl: "",
-//   },
-//   {
-//     artist: "Atrangi Funkaar",
-//     song: "Bam Bam",
-//     Spotify:
-//       "https://open.spotify.com/track/7aB7RxMZWIrCaWrilxtgOd?si=00bdb501a19a4184",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/bam-bam/1719581900?i=1719581901",
-//     Youtube: "https://youtu.be/XDxrssKdbQ4?si=KJRCUiNsuHBhxL2V",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_155645_Spotify.jpg",
-//   },
-//   {
-//     artist: "Mayyank Solanki ,Doleshwar Raj, Param",
-//     song: "Kya Karu",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/IMG-20240410-WA0001.jpg",
-//   },
-//   {
-//     artist: "Mayyank Solanki ,Doleshwar Raj, Param",
-//     song: "Gumshuda",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     artist: "Mayyank Solanki ,Doleshwar Raj, Param",
-//     song: "Behte Rehte",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-// ];
-
-// export const songsSSD = [
-//   {
-//     song: "Gulabi",
-//     artist: "Param & Hahrshita Banker",
-//     Producer: "Akshay Agarwal",
-//     Mix: "Akshay Agarwal",
-//     Master: "Akshay Agarwal",
-//     Spotify:
-//       "https://open.spotify.com/track/1hS0Ku3Jmc0yfiI5LGpPWA?si=a48088c917d8459a",
-//     AppleMusic: "https://music.apple.com/in/album/gulabi-single/1717239805",
-//     Youtube: "https://youtu.be/N62dhG9vsyE?si=1_2F2Q_5LhFfCGl_",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_155246_Spotify.jpg",
-//   },
-//   {
-//     song: "Saajan bairiya",
-//     artist: "Priyanshh Srivavstava, Namish Narayan",
-//     Producer: "VikramAditya",
-//     Mix: "VikramAditya",
-//     Master: "VikramAditya",
-//     Spotify:
-//       "https://open.spotify.com/track/34ioU8r6HO4z8w2FMQ8ne8?si=59e8cebc767a4fbd",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/jiya-jaye-na-single/1497178934",
-//     Youtube: "https://youtu.be/bqjSLqeu0PI?si=APEX2dfdx2POsqpK",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Jiya Jaye Na",
-//     artist: "SSD",
-//     Producer: "Taresh Agarwal",
-//     Mix: "Taresh Agarwal",
-//     Master: "Taresh Agarwal",
-//     Spotify:
-//       "https://open.spotify.com/track/34ioU8r6HO4z8w2FMQ8ne8?si=59e8cebc767a4fbd",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/jiya-jaye-na-single/1497178934",
-//     Youtube: "https://youtu.be/bqjSLqeu0PI?si=APEX2dfdx2POsqpK",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_155015_Spotify.jpg",
-//   },
-//   {
-//     song: "Nasha Zindagi",
-//     artist: "Mayyank Solanki",
-//     Producer: "Mayyank Solanki",
-//     Mix: "Abhishek Singh",
-//     Master: "Abhishek Singh",
-//     Spotify:
-//       "https://open.spotify.com/track/4CmvIikZB6Rs0kClZXa3e4?si=a3f960bfc35f47db",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/nasha-zindagi/1689267666?i=1689267667",
-//     Youtube: "https://youtu.be/E2qH08K2fds?si=6YExpU9t0lv9U2D0",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_154805_Spotify.jpg",
-//   },
-//   {
-//     song: "Ulfat Ka Dariya",
-//     artist: "Mayyank Solanki",
-//     Producer: "Mayyank Solanki",
-//     Mix: "Abhishek Singh",
-//     Master: "Abhishek Singh",
-//     Spotify:
-//       "https://open.spotify.com/track/1xRdztEhpLdQF2toubUmyw?si=c4b6cc41661c4c3f",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/ulfat-ka-dariya-single/1698511084",
-//     Youtube: "https://youtu.be/dm_P35Xtp44?si=iSWJNjP4xLPQtxTK",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_155043_Spotify.jpg",
-//   },
-//   {
-//     song: "Maula",
-//     artist: "Ajay Tewari",
-//     Producer: "Akshay Agarwal",
-//     Mix: "Abhishek Gautam",
-//     Master: "Abhishek Gautam",
-//     Spotify:
-//       "https://open.spotify.com/track/3Uv8wd1cEMxSZ1aPZxaEIZ?si=01290a03bbb64bbe",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/maula/1709279896?i=1709279898",
-//     Youtube: "https://youtu.be/M28doogxUuQ?si=bur761KwkSRHN554",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_154933_Spotify.jpg",
-//   },
-//   {
-//     song: "Tu Paas Hai",
-//     artist: "Diptanshu Tiwari",
-//     Producer: "Taresh Agarwal",
-//     Mix: "Taresh Agarwal",
-//     Master: "Taresh Agarwal",
-//     Spotify:
-//       "https://open.spotify.com/track/16o2ASTSiJOpHp225xHvPI?si=77f254d1864f4ffd",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/tu-paas-hai-single/1727501511",
-//     Youtube: "https://youtu.be/CkNT3cKcjBs?si=34NWJFa624Glmzqs",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_154836_Spotify.jpg",
-//   },
-//   {
-//     song: "Tu Paas Hai (Reprise)",
-//     artist: "Diptanshu Tiwari",
-//     Producer: "Taresh Agarwal",
-//     Mix: "Taresh Agarwal",
-//     Master: "Taresh Agarwal",
-//     Spotify:
-//       "https://open.spotify.com/track/2j5ISZp3eceysF1qLg3wGO?si=93c07d4cb1da4a87",
-//     AppleMusic: "",
-//     Youtube: "https://youtu.be/7_DIjye3jVY?si=kbJQ8pq9z9N0Ujt9",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_155043_Spotify.jpg",
-//   },
-//   {
-//     song: "Andhera - P",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Rascal",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Piya Nirmohe",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Rozana",
-//     Producer: "Doleshwar Raj & Mayyank Solanki",
-//     Mix: "Shreedhar Sir, TSM Studios",
-//     Master: "Shreedhar Sir, TSM Studios",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "2 Lafz",
-//     Producer: "Doleshwar Raj & Mayyank Solanki",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Guftagoo",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Zindagi",
-//     Producer: "Doleshwar Raj & Mayyank Solanki",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-// ];
-
-// export const songsMAMA = [
-//   {
-//     song: "OCD - Meraki",
-//     artist: "Meraki",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/ocd1.jpg",
-//   },
-//   {
-//     song: "Shanti - Meraki",
-//     artist: "Meraki",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Zammidar - Meraki",
-//     artist: "Meraki",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Hisaab - Meraki",
-//     artist: "Meraki",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Kiddies - Fat Guyy",
-//     artist: "Fat Guyy",
-//     Spotify:
-//       "https://open.spotify.com/track/0RuUKK6PciL1af8euocNQD?si=b8b936332a8246a6",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/kiddies/1744394684?i=1744394685",
-//     Youtube:
-//       "https://music.youtube.com/watch?v=3k7e0HFlWIs&si=IJekO8igcFYXpO_l",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_154021_YouTube_Music.jpg",
-//   },
-//   {
-//     song: "Zaalika - Fat Guyy",
-//     artist: "Fat Guyy",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Chaku - Fat Guyy",
-//     artist: "Fat Guyy",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Rage - Fat Guyy",
-//     artist: "Fat Guyy",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Game - Cira",
-//     artist: "Cira",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-//   {
-//     song: "Teri Aankhon Se - Abhishek Nirwan",
-//     artist: "Mama Beats",
-//     Producer: "Doleshwar Raj",
-//     Mix: "Avnesh Singh",
-//     Master: "Avnesh Singh",
-//     Spotify:
-//       "https://open.spotify.com/track/7qSJVSOA1pfvEY9zg2mnLn?si=3995058f111b47ec",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/teri-aankhon-se-single/1743919250",
-//     Youtube:
-//       "https://music.youtube.com/watch?v=BybtCP5_Uz0&si=hX6pZ55gw05Vwmej",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/IMG-20240418-WA0000.jpg",
-//   },
-//   {
-//     song: "Another World - Shunyaum",
-//     artist: "Mama Beats",
-//     Producer: "Doleshwar Raj",
-//     Mix: "Avnesh Singh",
-//     Master: "Avnesh Singh",
-//     Spotify:
-//       "https://open.spotify.com/track/6aNU7o6no73jxZGLFdNrFF?si=c8f117462596478f",
-//     AppleMusic:
-//       "https://music.apple.com/in/album/another-world-single/1737482775",
-//     Youtube:
-//       "https://music.youtube.com/watch?v=JhD_uoAvxfg&si=JjjVMkMvPNf_YlJg",
-//     imageUrl:
-//       process.env.NEXT_PUBLIC_API_URL  + "/SmartSelect_20240505_154106_YouTube_Music.jpg",
-//   },
-//   {
-//     song: "Paradise - Shunyaum - P",
-//     artist: "Mama Beats",
-//     Spotify: "",
-//     AppleMusic: "",
-//     Youtube: "",
-//     imageUrl: "",
-//   },
-// ];
 
 export const dollsImages = [
   process.env.NEXT_PUBLIC_API_URL + "/dolls1.jpeg",
