ccb6bfef15c3d72e2834f32ba02e395a76001e1b
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-10-26 18:54:18 +0530

    components changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -59,7 +59,7 @@ const webItems = [
 const Footer = () => {
   return (
     <footer className="bg-black">
-      <div className=" max-w-screen bg-black px-4 pt-8 sm:px-6 lg:px-[5%]">
+      <div className=" max-w-screen bg-black px-4 sm:px-6 lg:px-[5%]">
         <div className="border-t-[1px] border-[#c7c7c7]/30 mt-6 py-4 flex flex-col-reverse items-center  lg:flex-row lg:justify-between">
           <div className="w-3/12" />
           <p className="text-center text-sm text-quirkyReddishOrange">
