3b39fdd245ccd63d3e82e73ec0048e15ae737cc8
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-10-28 09:17:24 +0530

    minor fix:

--- app/page.jsx
+++ app/page.jsx
@@ -221,7 +221,7 @@ const Home = () => {
                     </span>
                   ))}
                 </h1>
-                <div className="flex flex-row justify-between items-start  w-9/12 lg:w-full mt-4">
+                <div className="flex flex-row justify-center lg:justify-start gap-x-8 items-start  w-9/12 lg:w-full mt-4">
                   {list.map((item, index) => (
                     <Link
                       // onMouseEnter={() => {
