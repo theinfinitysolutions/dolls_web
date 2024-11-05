f9dfd7ee5939fc58ae713dc8ccbd9ef33387d39f
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-11-05 19:38:19 +0530

    adding layout files

---
+++ app/exclusive/layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const metadata = {
+  title: "Exclusive | Dole's Music",
+  description: "We are creative music collective based in Mumbai, India.",
+};
+
+const Layout = ({ children }) => {
+  return (
+    <main className="flex flex-row justify-between h-full w-full">
+      {children}
+    </main>
+  );
+};
+
+export default Layout;
