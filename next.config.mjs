2576133b9bc761cb573c81c4bff2685dd5d51f8c
anirudhj2000 <anirudhjoshi985@gmail.com>
2025-01-01 15:40:05 +0530

    minor config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
   trailingSlash: true,
+  reactStrictMode: false,
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "d2g3h1gpjmm5ra.cloudfront.net",
+        protocol: 'https',
+        hostname: 'd2g3h1gpjmm5ra.cloudfront.net',
       },
       {
-        protocol: "https",
-        hostname: "d3rbf9e30ll7p5.cloudfront.net",
+        protocol: 'https',
+        hostname: 'd3rbf9e30ll7p5.cloudfront.net',
       },
       {
-        protocol: "https",
-        hostname: "d26cxpggxycgjx.cloudfront.net",
+        protocol: 'https',
+        hostname: 'd26cxpggxycgjx.cloudfront.net',
       },
       {
-        protocol: "https",
-        hostname: "mystorage1.blr1.cdn.digitaloceanspaces.com",
+        protocol: 'https',
+        hostname: 'mystorage1.blr1.cdn.digitaloceanspaces.com',
       },
     ],
     unoptimized: true,
@@ -26,17 +27,17 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/:all*(mp3)",
+        source: '/:all*(mp3)',
         headers: [
           {
-            key: "Content-Type",
-            value: "audio/mpeg", // Adjust as necessary
+            key: 'Content-Type',
+            value: 'audio/mpeg', // Adjust as necessary
           },
         ],
       },
     ];
   },
-  transpilePackages: ["three"],
+  transpilePackages: ['three'],
 };
 
 export default nextConfig;
