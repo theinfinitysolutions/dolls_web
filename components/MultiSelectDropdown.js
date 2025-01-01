fb1ae6488cbceb4b116c5d1aff0a53ecfe93eba3
anirudhj2000 <anirudhjoshi985@gmail.com>
2025-01-01 15:42:17 +0530

    added minor fixes for dropdowns

--- components/MultiSelectDropdown.js
+++ components/MultiSelectDropdown.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { useController, useForm } from "react-hook-form";
-import Select from "react-select";
+import React from 'react';
+import { useController, useForm } from 'react-hook-form';
+import Select from 'react-select';
 
 const services = [
-  { label: "Music Production", value: "Music Production" },
-  { label: "Mix-Master", value: "Mix-Master" },
-  { label: "AD Jingle", value: "AD Jingle" },
-  { label: "Callertunes/Ringtones", value: "Callertunes/Ringtones" },
-  { label: "Voice Over", value: "Voice Over" },
-  { label: "Post Production", value: "Post Production" },
-  { label: "Multilingual AD Jingles", value: "Multilingual AD Jingles" },
-  { label: "Others", value: "Others" },
+  { label: 'Music Production', value: 'Music Production' },
+  { label: 'Mix-Master', value: 'Mix-Master' },
+  { label: 'AD Jingle', value: 'AD Jingle' },
+  { label: 'Callertunes/Ringtones', value: 'Callertunes/Ringtones' },
+  { label: 'Voice Over', value: 'Voice Over' },
+  { label: 'Post Production', value: 'Post Production' },
+  { label: 'Multilingual AD Jingles', value: 'Multilingual AD Jingles' },
+  { label: 'Others', value: 'Others' },
 ];
 
 const MultiSelectDropdown = ({ name, control }) => {
@@ -25,9 +25,7 @@ const MultiSelectDropdown = ({ name, control }) => {
     onChange(selectedOptions.map((option) => option.value));
   };
 
-  const selectedValues = services.filter((option) =>
-    value?.includes(option.value)
-  );
+  const selectedValues = services.filter((option) => value?.includes(option.value));
 
   return (
     <Select
@@ -36,13 +34,13 @@ const MultiSelectDropdown = ({ name, control }) => {
       ref={ref}
       options={services}
       value={selectedValues}
-      placeholder="Purpose of Enquiry"
+      placeholder='Purpose of Enquiry'
       onChange={handleChange}
       styles={{
         placeholder: (defaultStyles) => {
           return {
             ...defaultStyles,
-            color: "rgb(255 255 255 / 0.8)",
+            color: 'rgb(255 255 255 / 0.8)',
           };
         },
         // container: (defaultStyles) => {
@@ -55,43 +53,43 @@ const MultiSelectDropdown = ({ name, control }) => {
         multiValueRemove: (baseStyles, state) => {
           return {
             ...baseStyles,
-            color: "#121212",
-            ":hover": {
-              color: "black",
+            color: '#121212',
+            ':hover': {
+              color: 'black',
             },
           };
         },
         menu: (baseStyles, state) => {
           return {
             ...baseStyles,
-            backgroundColor: "black",
+            backgroundColor: 'black',
             zIndex: 102,
           };
         },
         valueContainer: (baseStyles, state) => {
           return {
             ...baseStyles,
-            backgroundColor: "transparent",
-            paddingLeft: "0px",
+            backgroundColor: 'transparent',
+            paddingLeft: '0px',
             zIndex: 10,
           };
         },
         control: (baseStyles, state) => ({
           ...baseStyles,
-          backgroundColor: "transparent",
-          border: "none",
-          fontSize: "1.25rem",
-          paddingLeft: "0",
+          backgroundColor: 'transparent',
+          border: 'none',
+          fontSize: '1.25rem',
+          paddingLeft: '0',
         }),
         option: (baseStyles, state) => ({
           ...baseStyles,
-          backgroundColor: "black",
-          color: "white",
-          zIndex: 10,
-          fontSize: "1rem",
+          backgroundColor: 'black',
+          color: 'white',
+          zIndex: 7,
+          fontSize: '1rem',
         }),
       }}
-      className=" react-select mb-4 z-30 w-full bg-transparent placeholder:text-white/70 focus:bg-transparent text-white/90  text-xl border-b-[1px] border-red-800"
+      className=' react-select mb-4 z-30 w-full bg-transparent placeholder:text-white/70 focus:bg-transparent text-white/90 ring-0  text-xl border-b-[1px] border-red-800'
     />
   );
 };
