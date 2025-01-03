a676eccf3b0801d835ba286ee112821f2311a3ec
anirudhj2000 <anirudhjoshi985@gmail.com>
2025-01-04 00:40:38 +0530

    contact us form changes

--- components/ContactUsComponent.jsx
+++ components/ContactUsComponent.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { alfa } from "@/app/layout";
-import RevealOnScroll from "./RevealOnScroll";
-import emailjs from "@emailjs/browser";
-import { useRouter } from "next/navigation";
-import { formatDate } from "@/app/page";
-import MultiSelectDropdown from "./MultiSelectDropdown";
-import axios from "axios";
-import BudgetDropdown from "./BudgetDropdown";
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { alfa } from '@/app/layout';
+import RevealOnScroll from './RevealOnScroll';
+import emailjs from '@emailjs/browser';
+import { useRouter } from 'next/navigation';
+import { formatDate } from '@/app/page';
+import MultiSelectDropdown from './MultiSelectDropdown';
+import axios from 'axios';
+import BudgetDropdown from './BudgetDropdown';
 
 const ContactUsComponent = () => {
   const [loading, setLoading] = React.useState(false);
@@ -26,21 +26,21 @@ const ContactUsComponent = () => {
     setValue,
   } = useForm({
     defaultValues: {
-      name: "",
-      email: "",
-      phoneNumber: "",
+      name: '',
+      email: '',
+      phoneNumber: '',
       purpose: [],
-      budget: "",
-      message: "",
+      budget: '',
+      message: '',
     },
   });
 
-  const purpose = watch("purpose");
-  const budget = watch("budget");
+  const purpose = watch('purpose');
+  const budget = watch('budget');
 
   useEffect(() => {
-    console.log("bidget", budget);
-    if (purpose.length > 1 || purpose.includes("Music Production")) {
+    console.log('bidget', budget);
+    if (purpose.length > 1 || purpose.includes('Music Production')) {
       setIsBudgetDropdown(true);
       // setValue("budget", "");
     } else {
@@ -61,9 +61,9 @@ const ContactUsComponent = () => {
               fields: {
                 Name: data.name,
                 Email: data.email,
-                "Phone Number": data.phoneNumber,
-                "Purpose of Enquiry": data.purpose.toString(),
-                Budget: "₹" + data.budget,
+                'Phone Number': data.phoneNumber,
+                'Purpose of Enquiry': data.purpose.toString(),
+                Budget: '₹' + data.budget,
                 Message: data.message,
                 CreatedOn: formatDate(new Date()),
               },
@@ -72,27 +72,27 @@ const ContactUsComponent = () => {
         },
         {
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
             Authorization: `Bearer ${process.env.NEXT_PUBLIC_AIRTABLE_API_KEY}`,
           },
         }
       )
       .then((res) => {
-        if (res.status.toString()[0] !== "2") {
+        if (res.status.toString()[0] !== '2') {
           throw new Error(res);
         }
 
-        console.log("resres", res);
+        console.log('resres', res);
         setLoading(false);
         setEmailSent(true);
-        window.fbq("trackCustom", "Doles Lead", {
-          event: "Lead",
+        window.fbq('trackCustom', 'Doles Lead', {
+          event: 'Lead',
         });
         reset();
       })
       .catch((err) => {
         setLoading(false);
-        console.log("error", err);
+        console.log('error', err);
       });
   };
 
@@ -105,122 +105,98 @@ const ContactUsComponent = () => {
   }, [emailSent]);
 
   return (
-    <div className="flex flex-col w-full h-full items-center relative justify-center py-[2.5vh] ">
-      <div
-        className={
-          "flex flex-col  items-center justify-center w-full p-4 animate-animateSlideUp"
-        }
-      >
-        <h2
-          className={`${alfa.className} text-white text-[2.5rem] lg:text-[4rem] leading-[4rem] font-bold `}
-        >
+    <div className='flex flex-col w-full h-full items-center relative justify-center py-[2.5vh] '>
+      <div className={'flex flex-col  items-center justify-center w-full p-4 animate-animateSlideUp'}>
+        <h2 className={`${alfa.className} text-white text-[2.5rem] lg:text-[4rem] leading-[4rem] font-bold `}>
           CONTACT
         </h2>
-        <p className="text-white text-center text-sm">
-          {" Apply here to work with us"}
-        </p>
+        <p className='text-white text-center text-sm'>{' Apply here to work with us'}</p>
       </div>
-      <div className=" flex flex-col items-center w-full mt-4 lg:mt-[5vh]">
+      <div className=' flex flex-col items-center w-full mt-4 lg:mt-[5vh]'>
         <form
-          id="contact-form"
+          id='contact-form'
           onSubmit={handleSubmit(onSubmit)}
-          className=" w-[80vw] lg:w-[40%] flex flex-col items-center justify-center"
+          className=' w-[80vw] lg:w-[40%] flex flex-col items-center justify-center'
         >
-          <div className="flex flex-col items-start w-full">
+          <div className='flex flex-col items-start w-full'>
             <input
-              type="text"
-              id="name"
-              placeholder="Name*"
-              {...register("name", { required: true })}
-              className="mb-4 w-full bg-transparent placeholder:text-white/80 focus:bg-transparent text-white/90  text-xl border-b-[1px] border-red-800"
+              type='text'
+              id='name'
+              placeholder='Name*'
+              {...register('name', { required: true })}
+              className='mb-4 w-full bg-transparent placeholder:text-white/80 focus:bg-transparent text-white/90  text-xl border-b-[1px] border-red-800'
             />
-            {errors.name?.message ? (
-              <p className=" text-xs text-red-500">{errors.name.message}</p>
-            ) : null}
+            {errors.name?.message ? <p className=' text-xs text-red-500'>{errors.name.message}</p> : null}
           </div>
-          <div className="flex flex-col items-start w-full mt-2">
+          <div className='flex flex-col items-start w-full mt-2'>
             <input
-              id="email"
-              type="email"
-              placeholder="Email*"
-              {...register("email", {
+              id='email'
+              type='email'
+              placeholder='Email*'
+              {...register('email', {
                 required: true,
                 minLength: 5,
                 pattern: {
                   value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: "invalid email address",
+                  message: 'invalid email address',
                 },
               })}
-              className="mb-4 w-full placeholder:text-white/80 focus:bg-transparent text-white/90  bg-transparent text-xl border-b-[1px] border-red-800"
+              className='mb-4 w-full placeholder:text-white/80 focus:bg-transparent text-white/90  bg-transparent text-xl border-b-[1px] border-red-800'
             />
-            {errors.email?.message ? (
-              <p className=" text-xs text-red-500">{errors.email.message}</p>
-            ) : null}
+            {errors.email?.message ? <p className=' text-xs text-red-500'>{errors.email.message}</p> : null}
           </div>
-          <div className="flex flex-col items-start w-full mt-2">
+          <div className='flex flex-col items-start w-full mt-2'>
             <input
-              id="phoneNumber"
-              type="phoneNumber"
-              placeholder="Phone Number"
-              {...register("phoneNumber")}
-              className="mb-4 w-full placeholder:text-white/80 focus:bg-transparent text-white/90  bg-transparent text-xl border-b-[1px] border-red-800"
+              id='phoneNumber'
+              type='phoneNumber'
+              placeholder='Phone Number'
+              {...register('phoneNumber')}
+              className='mb-4 w-full placeholder:text-white/80 focus:bg-transparent text-white/90  bg-transparent text-xl border-b-[1px] border-red-800'
             />
           </div>
-          <div className="flex flex-col items-start w-full mt-2">
-            <MultiSelectDropdown name={"purpose"} control={control} />
-            {errors.purpose?.message ? (
-              <p className=" text-xs text-red-500">{errors.purpose.message}</p>
-            ) : null}
+          <div className='flex flex-col items-start w-full mt-2'>
+            <MultiSelectDropdown name={'purpose'} control={control} />
+            {errors.purpose?.message ? <p className=' text-xs text-red-500'>{errors.purpose.message}</p> : null}
           </div>
-          <div className="flex flex-col items-start w-full mt-2">
+          <div className='flex flex-col items-start w-full mt-2'>
             {isBudgetDropdown ? (
-              <BudgetDropdown name={"budget"} control={control} />
+              <BudgetDropdown name={'budget'} control={control} />
             ) : (
               <input
-                id="budget"
-                type={"number"}
-                placeholder="Budget"
-                {...register("budget")}
-                className="mb-4 w-full placeholder:text-white/80 focus:bg-transparent text-white/90  bg-transparent text-xl border-b-[1px] border-red-800"
+                id='budget'
+                type={'number'}
+                placeholder='Budget'
+                {...register('budget')}
+                className='mb-4 w-full placeholder:text-white/80 focus:bg-transparent text-white/90  bg-transparent text-xl border-b-[1px] border-red-800'
               />
             )}
           </div>
 
-          <div className="flex flex-col items-start w-full mt-2">
+          <div className='flex flex-col items-start w-full mt-2'>
             <textarea
-              id="message"
-              placeholder={`Message ${messageRequired ? "*" : ""}`}
-              {...register("message", {
+              id='message'
+              placeholder={`Message ${messageRequired ? '*' : ''}`}
+              {...register('message', {
                 required: messageRequired,
               })}
-              className="mb-4 w-full placeholder:text-white/80 focus:bg-transparent text-white/90  h-[10vh] bg-transparent text-xl border-b-[1px] border-red-800"
+              className='mb-4 w-full placeholder:text-white/80 focus:bg-transparent text-white/90  h-[10vh] bg-transparent text-xl border-b-[1px] border-red-800'
             />
-            {errors.message?.message ? (
-              <p className=" text-xs text-red-500">{errors.message.message}</p>
-            ) : null}
+            {errors.message?.message ? <p className=' text-xs text-red-500'>{errors.message.message}</p> : null}
           </div>
           {emailSent ? (
-            <div
-              type="submit"
-              disabled
-              className="bg-green-400 z-0 text-white px-8 py-2 mt-4 "
-            >
+            <div type='submit' disabled className='bg-green-400 z-0 text-white px-8 py-2 mt-4 '>
               Email Sent
+            </div>
+          ) : loading ? (
+            <div className='bg-red-800 disabled:bg-gray-700 text-white px-8 py-2 mt-2 z-10 lg:mt-8 cursor-pointer '>
+              {'...'}
             </div>
           ) : (
             <button
-              // disabled={!errors.name || !errors.email}
-              type="submit"
-              // onMouseEnter={() => {
-              //   setCurrentPointer("a");
-              // }}
-              // onMouseLeave={() => {
-              //   setCurrentPointer("");
-              // }}
-
-              className="bg-red-800 disabled:bg-gray-700 text-white px-8 py-2 mt-2 z-10 lg:mt-8 cursor-pointer "
+              type='submit'
+              className='bg-red-800 disabled:bg-gray-700 text-white px-8 py-2 mt-2 z-10 lg:mt-8 cursor-pointer '
             >
-              {loading ? "..." : "Submit"}
+              {'Submit'}
             </button>
           )}
         </form>
